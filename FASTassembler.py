60bfc2f446dccd717424a8bc9b0f9c5d3ce660be
Peter <peter.panourgias@gmail.com>
2017-11-18 14:15:58 -0800

    Complete?

--- FASTassembler.py
+++ FASTassembler.py
@@ -11,11 +11,11 @@ def get_opcode(op):
 	elif op == 'and':  return ('r', '0101')
 	elif op == 'sll':  return ('r', '0110')
 	elif op == 'srl':  return ('r', '0111')
-	elif op == 'lb':   return ('r', '1000')
-	elif op == 'sb':   return ('r', '1001')
+	elif op == 'lb':   return ('rmem', '1000')
+	elif op == 'sb':   return ('rmem', '1001')
 	elif op == 'bne':  return ('r', '1010')
 	elif op == 'put':  return ('i', '1011')
-	elif op == 'mov':  return ('r', '1100')
+	elif op == 'mov':  return ('rmov', '1100')
 	elif op == 'halt': return ('h', '1101')
 
 	return None
@@ -30,7 +30,7 @@ def getReg(reg):
 	elif reg == '0':   return '0'
 	elif reg == '1':   return '1'
 
-	return None
+	return '0'
 
 def main():
 	fname = sys.argv[1] 
@@ -38,6 +38,7 @@ def main():
 	file = open(fname, 'r')
 
 	for line in file:
+		orig = line.strip('\n').strip('\t').strip(' ')
 		machine_code = ''
 
 		line = line.strip('\n').strip('\t').strip(' ')
@@ -49,19 +50,38 @@ def main():
 
 			if opcode == None: continue
 
-			machine_code += opcode[1]
+			machine_code += opcode[1] + '_'
 
+			inst_type = opcode[0]
 
+			if inst_type == 'r':
+				machine_code += getReg(line[1])
+				machine_code += getReg(line[2])
+				machine_code += getReg(line[3])
 
+			elif inst_type == 'rmem':
+				machine_code += getReg(line[1]) + '00'
+				machine_code += getReg(line[2])
 
+			elif inst_type == 'rmov':
+				machine_code += getReg(line[1])
+				machine_code += getReg(line[2]) + '0'
 
+			elif inst_type == 'i':
+				imm = int(line[1].strip(','))
 
+				for i in range(3,-1,-1):
+					if ( (imm >> i) & 1 ) == 1:
+						machine_code += '1'
+					else:
+						machine_code += '0'
+
+				machine_code += getReg(line[2])
+
+			elif inst_type == 'h':
+				machine_code += '00000'
 
 			print machine_code
-
-
-		
-
 
 if __name__ == "__main__":
 	main()
